b867b4fa9275892dc1eb9ac05871e0daf075eff6
Harsha Chandwani <hachandw@microsoft.com>
2019-03-12 15:06:31 -0700

    c1

--- Hello.cs
+++ Hello.cs
@@ -5,7 +5,7 @@ namespace HelloWorld
     {
         static void Main() 
         {
-            Console.WriteLine("Hello harsha!");
+            Console.WriteLine("Hello harsha msft!");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");

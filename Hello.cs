c9b658cdfce69f0b0ce44a6980f5845b308ce75b
Harsha Chandwani <hachandw@microsoft.com>
2019-03-12 14:54:12 -0700

    hello from test

--- Hello.cs
+++ Hello.cs
@@ -5,7 +5,7 @@ namespace HelloWorld
     {
         static void Main() 
         {
-            Console.WriteLine("Hello MSFT!");
+            Console.WriteLine("Hello from test!");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
